57366c35afaef158816adf6d07c0b8e3663e6b3c
Joe Thorley <joe@poissonconsulting.ca>
2015-12-31 18:33:45 -0800

    Rationalised Testing

--- R/check.R
+++ R/check.R
@@ -1,13 +1,14 @@
 check_stop <- function(...) stop(..., call. = FALSE)
 
 check_section <- function(section) {
-  values <- list(Section = c(1L, .Machine$integer.max),
+  values <- list(Section = c(1L, nrow(section)),
                  SectionArea = c(0, Inf),
                  SectionX = 1,
                  SectionY = 1)
 
   datacheckr::check_data(section, values)
-  TRUE
+  stopifnot(!anyDuplicated(section$Section))
+  invisible(section)
 }
 
 check_section_polygons <- function(section_polygons) {
@@ -15,9 +16,12 @@ check_section_polygons <- function(section_polygons) {
   if (!inherits(section_polygons, "SpatialPolygonsDataFrame"))
     check_stop("section_polygons must be a spatial polygons data frame")
 
-  values <- list(Section = c(1L, .Machine$integer.max))
+  values <- list(Section = c(1L, nrow(section)))
+
   datacheckr::check_data(section_polygons@data, values)
-  TRUE
+  stopifnot(!anyDuplicated(section_polygons$Section))
+
+  invisible(section_polygons)
 }
 
 check_section_distance <- function(section_distance) {
@@ -26,77 +30,106 @@ check_section_distance <- function(section_distance) {
 
   if (!is.integer(section_distance))
     check_stop("section_distance must be an integer matrix")
-  TRUE
+  stopifnot(nrow(section_distance) == ncol(section_distance))
+
+  invisible(section_distance)
 }
 
 check_station <- function(station) {
-  values <- list(Station = c(1L, .Machine$integer.max),
-                 Section = c(1L, .Machine$integer.max),
+  values <- list(Station = c(1L, nrow(section)),
+                 Section = c(1L, datacheckr::max_integer()),
                  StationX = 1,
                  StationY = 1)
 
   datacheckr::check_data(station, values)
-  TRUE
+  invisible(station)
 }
 
 check_station_deployment <- function(station_deployment) {
-  values <- list(Station = c(1L, .Machine$integer.max),
-                 Receiver = c(1L, .Machine$integer.max),
+  values <- list(Station = c(1L, datacheckr::max_integer()),
+                 Receiver = c(1L, datacheckr::max_integer()),
                  ReceiverDateTimeIn = Sys.time(),
                  ReceiverDateTimeOut = Sys.time())
 
   datacheckr::check_data(station_deployment, values)
-  TRUE
 }
 
 check_recapture <- function(recapture) {
   values <- list(RecaptureDateTime = Sys.time(),
-                 Capture = c(1L, .Machine$integer.max),
-                 Section = c(1L, .Machine$integer.max),
+                 Capture = c(1L, datacheckr::max_integer()),
+                 Section = c(1L, datacheckr::max_integer()),
                  TBarTag1 = TRUE,
                  TBarTag2 = TRUE,
                  TagsRemoved = TRUE,
                  Released = TRUE)
 
   datacheckr::check_data(recapture, values)
-  TRUE
 }
 
 check_detection <- function(detection) {
 
   values <- list(DetectionDateTime = Sys.time(),
-                 Capture = c(1L, .Machine$integer.max),
-                 Receiver = c(1L, .Machine$integer.max),
-                 Detections = c(1L, .Machine$integer.max))
+                 Capture = c(1L, datacheckr::max_integer()),
+                 Receiver = c(1L, datacheckr::max_integer()),
+                 Detections = c(1L, datacheckr::max_integer()))
 
   datacheckr::check_data(detection, values)
-  TRUE
 }
 
 check_depth <- function(depth) {
 
   values <- list(
     DepthDateTime = Sys.time(),
-    Capture = c(1L, .Machine$integer.max),
-    Receiver = c(1L, .Machine$integer.max),
+    Capture = c(1L, datacheckr::max_integer()),
+    Receiver = c(1L, datacheckr::max_integer()),
     Depth = c(0, Inf))
+
   datacheckr::check_data(depth, values)
-  TRUE
 }
 
 check_capture <- function(capture) {
   values <- list(
-    Capture = c(1L, .Machine$integer.max),
+    Capture = c(1L, nrow(capture)),
     CaptureDateTime = Sys.time(),
-    Section = c(1L, .Machine$integer.max),
+    Section = c(1L, datacheckr::max_integer()),
     Species = factor(""),
     Length = c(1L, 1000L),
     Weight = c(0, Inf, NA),
-    Reward1 = c(1, 10, 100),
-    Reward2 = c(1, 10, 100),
+    Reward1 = c(1L, 10L, 100L),
+    Reward2 = c(1L, 10L, 100L),
     TagExpireDateTime = Sys.time())
 
-  TRUE
+  datacheckr::check_data(capture, values)
+  stopifnot(!anyDuplicated(capture$Capture))
+  invisible(capture)
+}
+
+check_all <- function(section, section_polygons, section_distance,
+                      station, station_deployment, recapture, detection, depth,
+                      capture) {
+
+   stopifnot(nrow(!section_polygons@data) == nrow(section))
+   stopifnot(nrow(section_distance) == nrow(section))
+   stopifnot(ncol(section_distance) == nrow(section))
+#   stopifnot()
+#   station %<>% dplyr::full_join(station_deployment, by = "Station")
+#   if (any(is.na(station$Station)))
+#     stop("unmatched values in column Station of station and station_deployment")
+#
+#   station %<>% dplyr::left_join(section, by = "Section")
+#   if (any(is.na(station$Section)))
+#     stop("unmatched values in column Section of station and section")
+#
+#   capture %<>% dplyr::full_join(recapture, by = "Capture")
+#   if (any(is.na(capture$Capture)))
+#     stop("unmatched values in column Capture of capture and recapture")
+#
+#   capture %<>% dplyr::rename_("Section = Section.x")
+#   capture %<>% dplyr::left_join(section, by = "Section")
+#   if (any(is.na(capture$Section)))
+#     stop("unmatched values in column Section of capture and section")
+
+    invisible(section)
 }
 
 #' Check Lake Exploitation Data
@@ -119,6 +152,9 @@ check_lex_data <- function(package) {
   check_detection(detection)
   check_depth(depth)
   check_capture(capture)
+  check_all(section, section_polygons, section_distance,
+                      station, station_deployment, recapture, detection, depth,
+                      capture)
 
   TRUE
 }
