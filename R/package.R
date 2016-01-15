9a7509fdfd9a40ba028464ab3d013d6e09f9ba1c
Joe Thorley <joe@poissonconsulting.ca>
2016-01-15 11:21:10 -0800

    Moved README to examples

--- R/package.R
+++ R/package.R
@@ -3,4 +3,16 @@
 #' @docType package
 #' @name lexr
 #' @importFrom magrittr %<>% %>%
+#' @examples
+#' library(dplyr)
+#' library(magrittr)
+#' library(qlexdatr)
+#' library(lexr)
+
+#' lex_data <- input_lex_data("qlexdatr")
+#' lex_data %<>% check_lex_data()
+
+#' print(lex_data)
+
+#' plot(lex_data)
 NULL
