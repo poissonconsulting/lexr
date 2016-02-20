399ce361614c2e3ed5e731b7e3a0e69221f50b0a
Joe Thorley <joe@poissonconsulting.ca>
2016-02-20 13:02:03 -0800

    plot all detects with color coded sections

--- R/plot-detect-data.R
+++ R/plot-detect-data.R
@@ -1,50 +1,15 @@
-plot_detect_coverage <- function(coverage, interval) {
-  coverage %<>% dplyr::inner_join(interval, by = "Interval")
-
-  coverage %<>% dplyr::mutate_(.dots = list(Year = "lubridate::year(Date)",
-                                            DayteTime = "DateTime"))
-  lubridate::year(coverage$DayteTime) <- 2000
-  ggplot2::ggplot(data = coverage, ggplot2::aes_(x = ~DayteTime,
-                                                 y = ~Coverage)) +
-    ggplot2::facet_grid(Section~Year) +
-    ggplot2::geom_area() +
-    ggplot2::scale_x_datetime(name = "Date", breaks = scales::date_breaks("6 months"), labels = scales::date_format("%b")) +
-    ggplot2::scale_y_continuous(name = "Coverage (%)", breaks = c(0.5,1), labels = scales::percent) +
-    ggplot2::expand_limits(y = c(0,1))
-}
-
-plot_detect_distance <- function(distance, section) {
-  distance %<>% dplyr::filter_(~Distance == 1L)
-  from <- inner_join(distance, section@data, by = c(SectionFrom = "Section"))
-  to <- inner_join(distance, section@data, by = c(SectionTo = "Section"))
-  to %<>% dplyr::select_(.dots = list(SectionTo = "SectionTo", EastingTo = "EastingSection",
-                                      NorthingTo = "NorthingSection"))
-  from %<>% dplyr::select_(.dots = list(SectionFrom = "SectionFrom", EastingFrom = "EastingSection",
-                                        NorthingFrom = "NorthingSection"))
-
-  distance <- dplyr::bind_cols(from, to)
-
-  ggplot2::ggplot(data = section@data, ggplot2::aes_(
-    x = ~EastingSection / 1000, y = ~NorthingSection / 1000)) +
-    ggplot2::geom_point() +
-    ggplot2::geom_segment(data = distance, ggplot2::aes_(
-      x = ~EastingFrom / 1000, y = ~NorthingFrom / 1000,
-      xend = ~EastingTo / 1000, yend = ~NorthingTo / 1000),
-      arrow = ggplot2::arrow(length = ggplot2::unit(0.1, "inches"), type = "closed"), alpha = 1/2) +
-    ggrepel::geom_text_repel(data = section@data, ggplot2::aes_(label = ~Section),
-                             size = 4) +
-    ggplot2::coord_equal() +
-    ggplot2::scale_x_continuous(name = "Easting (km)", labels = scales::comma) +
-    ggplot2::scale_y_continuous(name = "Northing (km)", labels = scales::comma)
-}
-
-plot_detect_section <- function(section) {
-
+section_polygon <- function(section) {
   polygon <- broom::tidy(section)
   polygon %<>% dplyr::rename_(.dots = list(Section = "id", EastingSection = "long",
                                            NorthingSection = "lat"))
   polygon$Section %<>% factor(levels = levels(section@data$Section))
   polygon %<>% dplyr::inner_join(dplyr::select_(section@data, ~-EastingSection, ~-NorthingSection), by = "Section")
+  polygon
+}
+
+plot_detect_section <- function(section) {
+
+  polygon <- section_polygon(section)
 
   ggplot2::ggplot(data = section@data, ggplot2::aes_(x = ~EastingSection / 1000,
                                                      y = ~NorthingSection / 1000,
@@ -61,6 +26,52 @@ plot_detect_section <- function(section) {
     ggplot2::scale_y_continuous(name = "Northing (km)", labels = scales::comma) +
     ggplot2::scale_color_identity() +
     ggplot2::scale_fill_identity()
+}
+
+plot_detect_coverage <- function(coverage, section, interval) {
+  tz <- lubridate::tz(interval$DateTime)
+  first_year <- lubridate::year(interval$DateTime[1])
+  last_year <- lubridate::year(interval$DateTime[nrow(interval)])
+
+  coverage %<>% dplyr::inner_join(interval, by = "Interval")
+  coverage %<>% dplyr::inner_join(section@data, by = "Section")
+
+  ggplot2::ggplot(data = coverage, ggplot2::aes_(x = ~DateTime,
+                                                 y = ~Coverage)) +
+    ggplot2::facet_grid(Section~.) +
+    ggplot2::geom_area(ggplot2::aes_(fill = ~ColorCode)) +
+    ggplot2::scale_x_datetime(name = "Date", expand = c(0,0), date_breaks = "1 year", date_labels = "%Y") +
+    ggplot2::scale_y_continuous(name = "Coverage (%)", breaks = c(0.5,1), labels = scales::percent) +
+    ggplot2::scale_fill_identity() +
+    ggplot2::expand_limits(x = as.POSIXct(paste0(c(first_year, last_year + 1), "-01-01"), tz = tz),
+                           y = c(0,1))
+}
+
+plot_detect_distance <- function(distance, section) {
+  distance %<>% dplyr::filter_(~Distance == 1L)
+  from <- inner_join(distance, section@data, by = c(SectionFrom = "Section"))
+  to <- inner_join(distance, section@data, by = c(SectionTo = "Section"))
+  to %<>% dplyr::select_(.dots = list(SectionTo = "SectionTo", EastingTo = "EastingSection",
+                                      NorthingTo = "NorthingSection"))
+  from %<>% dplyr::select_(.dots = list(SectionFrom = "SectionFrom", EastingFrom = "EastingSection",
+                                        NorthingFrom = "NorthingSection"))
+
+  distance <- dplyr::bind_cols(from, to)
+
+  ggplot2::ggplot(data = section@data, ggplot2::aes_(
+    x = ~EastingSection / 1000, y = ~NorthingSection / 1000)) +
+    ggplot2::geom_point(ggplot2::aes_(color = ~ColorCode), size = 4) +
+    ggplot2::geom_segment(data = distance, ggplot2::aes_(
+      x = ~EastingFrom / 1000, y = ~NorthingFrom / 1000,
+      xend = ~EastingTo / 1000, yend = ~NorthingTo / 1000),
+      arrow = ggplot2::arrow(length = ggplot2::unit(0.1, "inches"), type = "closed"), alpha = 1/2) +
+    ggrepel::geom_text_repel(data = section@data, ggplot2::aes_(label = ~Section),
+                             size = 4) +
+    ggplot2::coord_equal() +
+    ggplot2::scale_x_continuous(name = "Easting (km)", labels = scales::comma) +
+    ggplot2::scale_y_continuous(name = "Northing (km)", labels = scales::comma) +
+    ggplot2::scale_color_identity()
+
 }
 
 plot_detect_overview <- function(capture, recapture, detection, section, interval) {
@@ -104,7 +115,7 @@ plot_detect_overview <- function(capture, recapture, detection, section, interva
 #' @export
 plot.detect_data <- function(x, all = FALSE, ...) {
   print(plot_detect_section(x$section))
-  print(plot_detect_coverage(x$coverage, x$interval))
+  print(plot_detect_coverage(x$coverage, x$section, x$interval))
   print(plot_detect_distance(x$distance, x$section))
   print(plot_detect_overview(x$capture, x$recapture, x$detection, x$section, x$interval))
   if (all) {
